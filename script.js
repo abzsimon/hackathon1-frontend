6df8e86a501569b1add1fba9ba05ee886eebfff9
Willy-Thai <haruki.thai@gmail.com>
2024-11-06 00:19:57 +0100

    sauvegarde de la journee

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector("#homeButton").addEventListener("click", function () {
+    window.location.assign('index.html');
+})
+
+document.querySelector("#cartButton").addEventListener("click", function () {
+    window.location.assign('cart.html');
+})
+
+document.querySelector("#bookingsButton").addEventListener("click", function () {
+    window.location.assign('bookings.html');
+})
+
