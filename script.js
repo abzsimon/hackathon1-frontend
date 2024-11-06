34ab26b312f76a73244ae24e3b2980e98bd2ea44
Willy-Thai <haruki.thai@gmail.com>
2024-11-06 19:30:55 +0100

    fetch frontend

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ document.querySelector("#bookingsButton").addEventListener("click", function () 
 })
 
 
+
+
+// fetch('https://hackathon1-backend.vercel.app/trips')
+
+const departureInput = document.querySelector("#departureInput");
+const departureValue = departureInput.value;
+
+const arrivalInput = document.querySelector("#arrivalInput");
+const arrivalValue = arrivalInput.value;
+
+const dateInput = document.querySelector("#dateInput");
+const dateValue = dateInput.value;
+
+// fetch(`https://hackathon1-backend.vercel.app/trips?departure=${departureValue}&arrival=${arrivalValue}&date=${dateValue}`, {
+document.querySelector("#searchButton").addEventListener("click", function () {
+    fetch("https://hackathon1-backend.vercel.app/trips", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+            departure: departureValue, 
+            arrival: arrivalValue, 
+            date: dateValue })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+})
+
+
+    // fetch method post
