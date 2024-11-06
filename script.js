0dad417889f1e18ada0d2431679af91dbb7f6305
Willy-Thai <haruki.thai@gmail.com>
2024-11-06 20:16:28 +0100

    reparation fetch

--- script.js
+++ script.js
@@ -15,30 +15,39 @@ document.querySelector("#bookingsButton").addEventListener("click", function () 
 
 // fetch('https://hackathon1-backend.vercel.app/trips')
 
-const departureInput = document.querySelector("#departureInput");
-const departureValue = departureInput.value;
+// const departureInput = document.querySelector("#departureInput");
+// const departureValue = departureInput.value;
 
-const arrivalInput = document.querySelector("#arrivalInput");
-const arrivalValue = arrivalInput.value;
+// const arrivalInput = document.querySelector("#arrivalInput");
+// const arrivalValue = arrivalInput.value;
 
-const dateInput = document.querySelector("#dateInput");
-const dateValue = dateInput.value;
+// const dateInput = document.querySelector("#dateInput");
+// const dateValue = dateInput.value;
 
 // fetch(`https://hackathon1-backend.vercel.app/trips?departure=${departureValue}&arrival=${arrivalValue}&date=${dateValue}`, {
 document.querySelector("#searchButton").addEventListener("click", function () {
+    const departureInput = document.querySelector("#departureInput");
+    const departureValue = departureInput.value;
+
+    const arrivalInput = document.querySelector("#arrivalInput");
+    const arrivalValue = arrivalInput.value;
+
+    const dateInput = document.querySelector("#dateInput");
+    const dateValue = dateInput.value;
     fetch("https://hackathon1-backend.vercel.app/trips", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 
             departure: departureValue, 
             arrival: arrivalValue, 
-            date: dateValue })
+            date: new Date(dateValue) })
     })
     .then(response => response.json())
     .then(data => {
         console.log(data);
     })
 })
+
 
 
     // fetch method post
